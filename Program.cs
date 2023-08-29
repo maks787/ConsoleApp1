91938f90cc81e8f62fd86cb685f3194eb9a27471
opilane <opilane@HP-CZC2349HT2>
2023-08-29 16:27:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+namespace Students
+{
+    public class Student
+    {
+        public string Name { get; }
+        public int age { get; }
+        private readonly string _city;
+        public string status;
+
+        public Student(string name, int age, string city)
+        {
+            Name = name;
+            Age = age;
+            _city = city;
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                if (age < 18) status = "alaealine";
+                else status = "täislasvanud";
+            }
+        }
+        public string Status
+        {
+            get { return status; }
+        }
+        public string GetCity()
+        {
+            return _city;
+        }
+
+
+        public void ShowInfo()
+
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(GetCity());
+
+        }
+    }
+}
+
+
---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿namespace Students
+{
+    public class StudentsMain
+    {
+        public static void Main()
+        {
+            Student student = new Student("John", 17, "London");
+           
+            student.ShowInfo();
+                
+        }
+    }
+}
+
+
+
+
