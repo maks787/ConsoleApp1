11628700dbc99cf1f402b800c86eef3c9ce67e9d
Maksym Miskevych <120181270+maks787@users.noreply.github.com>
2023-09-05 14:27:28 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,53 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
-namespace Students
+
+namespace Students_Naididis
 {
-    public class Student
+    public class MessageMain
     {
-        public string Name { get; }
-        public int age { get; }
-        private readonly string _city;
-        public string status;
+        public static void Main()
+        {
+            Message m1 = new Message("Hello", "John", DateTime.Now.AddDays(-10));
+            m1.ShowMessageInfo();
+            m1.AddLike();
+            Console.WriteLine(m1.GetPopularity());
 
-        public Student(string name, int age, string city)
-        {
-            Name = name;
-            Age = age;
-            _city = city;
-        }
-        public int Age
-        {
-            get { return age; }
-            set
-            {
-                age = value;
-                if (age < 18) status = "alaealine";
-                else status = "täislasvanud";
-            }
-        }
-        public string Status
-        {
-            get { return status; }
-        }
-        public string GetCity()
-        {
-            return _city;
-        }
+            Message m2 = new Message("Hi", "Mary", DateTime.Now.AddMinutes(-1));
+            m2.ShowMessageInfo();
 
+            for (int i = 0; i < 1000; i++) { m2.AddLike(); }
 
-        public void ShowInfo()
-
-        {
-            Console.WriteLine(Name);
-            Console.WriteLine(Age);
-            Console.WriteLine(GetCity());
+            Console.WriteLine(m2.GetPopularity());
+            Console.WriteLine(m1.GetPopularityInfo(m1.GetPopularity(), m2.GetPopularity()));
+            Message m3 = new Message("Tere", "Mari", DateTime.Now.AddDays(-1));
+            Message m4 = new Message("Guten Tag", "Hans", DateTime.Now.AddDays(-5));
+            Message m5 = new Message("Hei", "Marika", DateTime.Now.AddDays(-10));
+            List<Message> list = new List<Message>();
+            list.Add(m1);
+            list.Add(m2);
+            list.Add(m3);
+            list.Add(m4);
+            list.Add(m5);
+            Console.WriteLine(m1.GetPopularityInfoN(list));
 
         }
     }
 }
-
-
